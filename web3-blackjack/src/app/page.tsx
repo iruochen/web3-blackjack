06ffda82bae87e2d3fca13c0aee74d8d977f5539
ruochen <wangrui@ruochen.email>
2025-08-05 22:05:39 +0800

    chore: initialize wagmi project

---
+++ web3-blackjack/src/app/page.tsx
@@ -0,0 +1,31 @@
+export default function Page() {
+	return (
+		<div>
+			<h1>Welcome to Web3 game Black jack</h1>
+			<h2>Message: Player wins / dealer wins: Blackjack / bust!</h2>
+			<div>
+				<h2>Dealer's hand</h2>
+				<div>
+					<div></div>
+					<div></div>
+					<div></div>
+				</div>
+			</div>
+
+			<div>
+				<h2></h2>
+				<div>
+					<div></div>
+					<div></div>
+					<div></div>
+				</div>
+			</div>
+
+			<div>
+				<button>Hit</button>
+				<button>Stand</button>
+				<button>Reset</button>
+			</div>
+		</div>
+	)
+}
